5611092aebdc1a4174d8ff0dd564a1e51b5a12c3
Ano <163797569+Naufalano@users.noreply.github.com>
2025-05-27 01:25:31 +0700

    fixed issue: energy drain while entering main house

--- MovingAction.java
+++ MovingAction.java
@@ -94,7 +94,7 @@ public class MovingAction extends Action {
                 String transitionType = isTransitionViaEdge ? "tepi peta" : "pintu";
                 System.out.println(player.getName() + " mencapai " + transitionType + " di " + activeMapForStep.getMapName() + " menuju " + destinationMapName + "...");
                 
-                if (player.getEnergy() >= VISIT_ENERGY_COST) {
+                if (player.getEnergy() >= VISIT_ENERGY_COST && !destinationMapName.equals("Player's House")) {
                     player.setEnergy(player.getEnergy() - VISIT_ENERGY_COST);
                     farm.advanceGameTime(VISIT_TIME_COST_MINUTES);
                     String oldLocationName = player.getCurrentLocationName();
